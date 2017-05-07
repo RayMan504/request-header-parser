889aa554dfd260a3e339f147b400b643c3eaefa2
Uraeus Ray Millet <uraeusmillet@gmail.com>
2017-05-07 22:32:09 +0000

    install useragent

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+// useragent module that identifies IP address, language and operating system for my browser
+const useragent = require('useragent');
 const app = express();
 
 app.get('/', function(req, res) {
@@ -8,3 +10,7 @@ app.get('/', function(req, res) {
 app.listen(8080, function() {
     console.log('Listening on port 8080');
 });
+
+app.get('/:address', function(req, res) {
+    
+})
